66e9457293928077c23d85434e3a0872bcf0843f
Ruben Rossbach <ruben.rossbach@gmx.net>
2022-09-19 23:42:05 +0200

    added progress chart

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
 
 st.set_page_config(
     page_title="Machine Learning Skill Test",
@@ -15,9 +16,11 @@ df = get_data()
 
 st.write("# Machine Learning Skill Test")
 
-# index session state
+# Session state
 if "index" not in st.session_state:
     st.session_state.index = 0
+if "correct" not in st.session_state:
+    st.session_state["correct"] = np.array([np.nan] * len(df))
 
 # Controls
 col1, col2, col3, col4 = st.columns(4)
@@ -67,17 +70,58 @@ with cols[2]:
     if st.button("Check"):
         if sel == correct_choice:
             st.success("Correct!")
+            if st.session_state.correct[st.session_state.index] != 0:
+                st.session_state.correct[st.session_state.index] = 1
         else:
             st.error("Wrong")
+            st.session_state.correct[st.session_state.index] = 0
 
+# Display progress
+num_wrong = (st.session_state.correct == 0).sum()
+num_correct = (st.session_state.correct == 1).sum()
+num_unanswered = len(df) - num_wrong - num_correct
+prog = pd.DataFrame({
+    "wrong": num_wrong,
+    "correct": num_correct,
+    "unanswered": num_unanswered
+    }, index=[0])
+
+st.write("### Progess")
+
+# Plotly
+prog = pd.DataFrame({
+    "value": [num_correct, num_wrong, num_unanswered],
+    "label": ["correct", "wrong", "unanswered"],
+    "y": [1,1,1]
+    }, index=[0,1,2])
+
+fig = px.bar(prog, x="value", y="y", color='label', orientation='h',
+             labels={"value": "", "y": ""},
+             hover_name="value", hover_data={"y": False},
+             height=180, color_discrete_map={
+                 "wrong": "#FF6666",
+                 "correct": "#00FF00",
+                 "unanswered": "#C0C0C0"})\
+                     .update_layout(legend={"orientation": "h",
+                                            "title": "",
+                                            "x": 0.25,
+                                            "y": -0.33},
+                                    xaxis={"range": (0, len(df)),
+                                           "tickmode": "array",
+                                           "tickvals": np.array([20,40,60,80,len(df)])},
+                                    yaxis={"showticklabels": False},
+                                    plot_bgcolor="white")
+
+st.plotly_chart(fig)
+
+
+# Signature
+st.write("")
 st.write("")
 urlrLink = "[LinkedIn](https://www.linkedin.com/in/ruben-rossbach/)"
 urlrGH = "[Github](https://github.com/rubenrossbach)"
 st.write("Made by Ruben Rossbach")
 st.write(f"{urlrLink} | {urlrGH}")
 
-# push to Heroku
-
-# Add display of right and wrong (green, red, grey, horizontal)
 
 # Add retry of failed tests
